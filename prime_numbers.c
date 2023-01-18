1a1ba09e450ba6ba17187fbbe76cc39f90bd837e
Hayet <117082010+HayetFer@users.noreply.github.com>
2023-01-18 10:50:16 +0100

    Update prime_numbers.c

--- prime_numbers.c
+++ prime_numbers.c
@@ -27,20 +27,17 @@ int compter(int*tab){
 void tableau(){
     int* nbrPrems=malloc(taille*sizeof (int));
     nbrPrems[0]=2;
-    int courant = 3;
+    int courant = 2;
     int index = 0;
     for(int i=1;i<taille;++i){
-        if(estPrems(nbrPrems,courant,index))
-            nbrPrems[index++]=courant;
-        courant+=2;
+        courant++;
+        if(estPrems(nbrPrems,courant,index)==1){
+            index++;
+            nbrPrems[index]=courant;
+        }
     }
     /*for(int i=0;i<30 && nbrPrems[i]!=0;++i){
         printf("%d ",nbrPrems[i]);
     }*/
-    printf("%d\n",compter(nbrPrems));
-}
-
-int main() {
-    tableau();
-    return 0;
+    printf("%d ",compter(nbrPrems));
 }
